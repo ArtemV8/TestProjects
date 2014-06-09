65c6532a46b302d2f4194903f0665799c9306236
Artem V <artemv8@gmail.com>
2014-06-09 14:35:28 +0300

    swap2 is addded

--- JavaRush/src/References.java
+++ JavaRush/src/References.java
@@ -9,7 +9,7 @@ public class References
 		int n = 165;
 
 		System.out.println("M=" + m + " N=" + n);
-		swap(m, n);
+		swap2(m, n, m);
 		System.out.println("M=" + m + " N=" + n);
 	}
 	
@@ -18,5 +18,12 @@ public class References
 		int c = a;
 		a = b;
 		b = c;
+	}
+	
+	private static void swap2(int a, int b, int d)
+	{
+		int c = a;
+		a = d;
+		b = d;
 	}
 }
